ebc23196f3b2da4a2012df70abeea064ff97d3b1
artsilveira <164525185+artsilveira@users.noreply.github.com>
2026-06-15 21:27:06 -0300

    First commit - initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-white">Hello World!!!</h1>
+      <p className="text-gray-400 mt-2">Meu primeiro componente React!</p>
+    </div>
+  )
+}
+
+export default App
